852a17d6e06ef108ef7a1ec9d5ee506f327def2e
congiirepair <congiirepair@gmail.com>
2026-04-21 15:18:17 -0700

    Bump version to 2.3.8 for published release

--- ACS_Wynn_Builder.h
+++ ACS_Wynn_Builder.h
@@ -438,7 +438,7 @@ private:
     QNetworkAccessManager* downloadManager;
 
     QPushButton* btnUpdateApp = nullptr;
-    const QString CURRENT_VERSION = "2.3.7";
+    const QString CURRENT_VERSION = "2.3.8";
     void checkForUpdates(bool interactive = false);
 
     QNetworkReply* downloadReply = nullptr;
